e1e0567679ad3b0ad1f793bce93451d311388bb6
RomanGodMode <Poza228337>
2022-03-24 23:17:17 +0300

    add biljwater-rats task

--- src/ez_tasks/biljwater-rats.ts
+++ src/ez_tasks/biljwater-rats.ts
@@ -1,15 +1,16 @@
-const heights = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+export const calcWaterAmount = (heights: number[]): number => heights.reduce(
+  (waterAmount, height, index) => {
+    const leftBorder = Math.max(...heights.slice(0, index), 0)
+    const rightBorder = Math.max(...heights.slice(index + 1), 0)
 
-export const createMatrix = (heights: number[]): boolean[][] => {
-  const height = Math.max(...heights)
+    let amount = Math.min(leftBorder, rightBorder) - height
+    amount = Math.max(amount, 0)
 
-  return heights.map(h => [...Array(height)].map((_, idx) => idx < h))
-}
+    return waterAmount + amount
+  }, 0)
 
-console.log(createMatrix(heights))
+// const heights = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+const heights = [5, 0, 4, 0, 1, 5, 0, 3]
+console.log(calcWaterAmount(heights))
 
-export const calcWaterAmount = (heights: number[]): number => {
-
-
-  return 2
-}
+//https://www.youtube.com/watch?v=2xZq8z_A-NQ&list=WL&index=4&t=85s
